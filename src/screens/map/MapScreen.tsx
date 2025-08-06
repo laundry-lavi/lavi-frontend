e3a9b03157b4ec3bff2516a0c4c43400acfd4c47
Arthur Rolemberg <arthurrolemberg4262@gmail.com>
2025-08-05 23:41:32 -0300

    testando o mapa

--- src/screens/map/MapScreen.tsx
+++ src/screens/map/MapScreen.tsx
@@ -1,11 +1,13 @@
 import { View } from "react-native";
+import MapView from "react-native-maps";
+
 import { Text, Header, BottomSheet } from "@/components";
 
 export default function Map() {
   return (
-    <View>
+    <View className="flex-1">
       <Header />
-      <Text>Map Screen</Text>
+      <MapView style={{ flex: 1 }} showsUserLocation={true} />
       <BottomSheet />
     </View>
   );
