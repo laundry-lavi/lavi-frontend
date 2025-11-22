d735971453343074f62c96d1bcf4388c44c37181
https-dre <diaso.andre@outlook.com>
2025-11-22 19:48:56 -0300

    feat(socket.io): add suport to custom events

--- src/socket-io/index.ts
+++ src/socket-io/index.ts
@@ -1,7 +1,12 @@
 import { WS_URL } from "@/constants/backend";
 import { io, Socket } from "socket.io-client";
+import { notificationEvent } from "./events/notification";
 
 export const socket: Socket = io(WS_URL, {
   transports: ["websocket"], // <--- OBRIGATÓRIO: Remove o polling da lista
   autoConnect: false,
 });
+
+export const registerEvents = (io: Socket) => {
+  notificationEvent(io);
+}
