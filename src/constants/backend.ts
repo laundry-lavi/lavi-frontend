fd7cc12fb930f0121dcf73a3c8ef8691dde0822a
https-dre <diaso.andre@outlook.com>
2025-11-22 19:11:51 -0300

    chore: add socket.io url

--- src/constants/backend.ts
+++ src/constants/backend.ts
@@ -1,2 +1,7 @@
-const PROD_API = "https://illuminational-earlene-incoherently.ngrok-free.dev"
-export const API_URL = PROD_API;
+const PROD_API = "https://illuminational-earlene-incoherently.ngrok-free.dev";
+const DEV_API = "https://terminally-divine-amoeba.ngrok-free.app";
+const PROD_WS = "http://52.67.221.152:8081"
+const DEV_WS = "http://192.168.15.37:3355"
+
+export const API_URL = DEV_API;
+export const WS_URL = "https://weak-bears-hammer.loca.lt";
