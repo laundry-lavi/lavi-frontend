6138e72fb5dc2da929223c7570f043135cd324bc
https-dre <diaso.andre@outlook.com>
2025-11-22 19:48:27 -0300

    feat: add listener for server messages

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -1,25 +1,29 @@
 // contexts/SocketContext.tsx
-import React, { createContext, useContext, useEffect } from 'react';
-import { socket } from '@/socket-io';
+import React, { createContext, useContext, useEffect } from "react";
+import { registerEvents, socket } from "@/socket-io";
 
 const SocketContext = createContext(socket);
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
-    socket.connect();
-
-    socket.on('connect', () => {
-      console.log('Conectado ao Socket:', socket.id);
+    socket.on("connect", () => {
+      console.log("Conectado ao Socket:", socket.id);
     });
 
-    socket.on('disconnect', () => {
-      console.log('❌ Desconectado do Socket');
+    socket.on("disconnect", () => {
+      console.log("❌ Desconectado do Socket");
     });
 
-    socket.on('connect_error', (err) => {
-      console.log('Erro de conexão:', err.message);
+    socket.on("connect_error", (err) => {
+      console.log("Erro de conexão:", err.message);
       throw err;
     });
+
+    socket.on("from-server", (data) => {
+      console.log(`Mensagem do Servidor `);
+      console.log(data);
+    });
+    registerEvents(socket);
 
     return () => {
       socket.disconnect();
@@ -27,9 +31,7 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   }, []);
 
   return (
-    <SocketContext.Provider value={socket}>
-      {children}
-    </SocketContext.Provider>
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
 };
 
